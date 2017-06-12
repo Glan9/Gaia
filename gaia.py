ec9ac1cc81da719d4373f79f27062eaeadb0c66b
Sam Curran <splcurran@hotmail.com>
2017-06-11 20:10:53 -0400

    String parsing added

--- gaia.py
+++ gaia.py
@@ -37,7 +37,7 @@ else:
 
 lines = code.split('\n')
 
-print(lines)
+#print(lines)
 
 
 #line = lines[-1]
@@ -49,27 +49,48 @@ for line in lines:
 
 	while len(line) > 0:
 
-		if re.match("^-?(\d+(\.\d+)?|\.\d+)", line):
+		if re.match("^((\\[“”]|[^“])*)”", line):
+			# Match an unopened quote
+			#print("Unstarted quote")
+			string = re.match("^((\\[“”]|[^“])*)”", line).group(1)
+			func.append(operators.Operator(string, 0, ( lambda x: lambda stack: stack.append(x) )(string) ))
+			line = re.sub("^((\\[“”]|[^“])*)”", '', line)
+		elif re.match("^“[^”]*(”|$)", line):
+			# Match a normal or unfinished string
+			#print("Normal/unfinished quote")
+			string = re.match("^“([^”]*)(”|$)", line).group(1)
+			strings = string.split('“')
+			if len(strings) == 1:
+				func.append(operators.Operator(string, 0, ( lambda x: lambda stack: stack.append(x) )(strings[0]) ))
+			else:
+				func.append(operators.Operator(string, 0, ( lambda x: lambda stack: stack.append(x) )(strings) ))
+			line = re.sub("^“([^”]*)(”|$)", '', line)
+		elif re.match("^-?(\d+(\.\d+)?|\.\d+)", line):
+			# Match a number literal
+			#print("Number")
 			num = float(re.match("^-?(\d+(\.\d+)?|\.\d+)", line).group(0))
-			func.append(operators.Operator(str(num), 0, ( lambda x: lambda stack:stack.append(x) )(num) ))
+			func.append(operators.Operator(str(num), 0, ( lambda x: lambda stack: stack.append(x) )(num) ))
 			line = re.sub("^-?(\d+(\.\d+)?|\.\d+)", '', line)
 		elif line[0] in operators.ops:
+			# Match an operator
+			#print("Operator")
 			func.append(operators.ops[line[0]])
 			line = line[1:]
 		else:
+			#print("Other")
 			line = line[1:]
 
-	functions.append(func) ## TODO Fix whatever weird scoping or something making this not work
+	functions.append(func)
 
 
 ### MORE TESTING BELOW
-'''for op in func:
+
+#print(functions)
+
+'''for op in functions[-1]:
 	op.execute(stack)
 
-print(stack)
-print(line)'''
-
-print(functions)
+print(stack)'''
 
 #interpret(code, []) # Run the code starting with an empty stack
 
